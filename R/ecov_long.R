b1d825c39ddae2beeb87c3798a55fa46042d2ef3
Dustin Wood <84573697+funfield-lab@users.noreply.github.com>
2021-07-01 08:50:55 +0300

    better handling of 'did_i' feature in 'ecov_long'

--- R/ecov_long.R
+++ R/ecov_long.R
@@ -17,8 +17,8 @@
 #' @param p variable identifying 'person' (or respondent) - give in "quotemarks"
 #' @param s variable identifying 'situation' (or scenario) - give in "quotemarks"
 #' @param i variable identifying 'action' (or response) - give in "quotemarks"
-#' @param addDidi should you add the 'did_i' variable as element for effect covariance matrix?
-#' Defaults to \code{TRUE}.
+#' @param addDidi should you add the 'did_i' variable as element for effect covariance matrix,
+#' if not already present? Defaults to \code{TRUE}.
 #' @return Effect covariance elements
 #' @details
 #' Usage notes: eData should typically have format of \code{p}, \code{s}, \code{i}, as first three columns,
@@ -30,7 +30,8 @@
 ecov_long <- function(eData, p = "p", s = "s", i = "i",addDidi = T) {
 
 #0.  Add the 'did_i' column to the set before estimating covariance elements
-if(addDidi == T) {
+      #note: requires both option set to 'TRUE' and 'did_i' is not already present
+  if(addDidi == T & (('did_i' %in% colnames(eData)) == F)) {
   eData<-data.frame(eData[1:3],1,eData[4:ncol(eData)])
   colnames(eData)[4] <- "did_i"
 }
@@ -38,7 +39,7 @@ if(addDidi == T) {
   #1. Compute effectA*effectB elements.
   #data_long <- reshape2::melt(eData, id=c(p,s,i))
   data_long <- tidyr::pivot_longer(eData,cols = 4:ncol(eData),  names_to = "fX", values_to = "dX")
-  data_longer <-merge(data_long,eData,by.x = c(p,s,i))
+  data_longer <-merge(data_long,eData,by = c(p,s,i))
   eData_long <- tidyr::pivot_longer(data_longer,cols = 6:ncol(data_longer),  names_to = "fY", values_to = "dY")
   eData_long$dXdY<-eData_long$dX*eData_long$dY
   return(eData_long)
