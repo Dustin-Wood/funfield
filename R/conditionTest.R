c94b5a781fbf9bba502b75dcc2e0947678aedfe6
Dustin Wood <84573697+funfield-lab@users.noreply.github.com>
2021-10-29 14:32:58 +0300

    * change how location pointed to in 'strLine' function * now provide (s,i).Condition means in 'conditionTest' function

--- R/conditionTest.R
+++ R/conditionTest.R
@@ -7,9 +7,11 @@
 #' @param locEE location of expected outcome ratings to test for condition
 #' associations - usually something like \code{c(4:8)},
 #' as first three columns should be \code{p,s,i}
-#' @return Correlations and p-Values linking expected outcome
+#' @return Means, correlations and p-values linking expected outcome
 #' ratings to high/low condition levels
-#' @usage ecov(data,fX,fY,dXdY="cov")
+#' \item{siMeans}{Situation-Action mean ratings, given in separate dataframe by condition}
+#' \item{rXCond}{correlation between expected effect (X) and condition (L=-1,H=1)}
+#' \item{pXCond}{statistical significance of these correlations (or mean difference, which is equivalent, btw)}
 #' @details This assumes a very regimented c("p","s","i",...) data structure.
 #' Additionally, the code is currently only for dichotomous condition levels.  If a
 #' condition has more than 2 factor levels, then a different function will have to be
@@ -17,20 +19,27 @@
 #' @export
 
 
+
 conditionTest <- function(PSIdata.cond, hiCond, loCond, locEE){
+  #remove any other labels that might be in 'condition' variable
   PSIdata.HL <- subset(PSIdata.cond, (condition == hiCond | condition == loCond))
 
-  #recode 'subordinate' and 'manager' into -1 and 1, respectively, and make numeric variable
+  #estimate means by (s,i,H/L)
+  siMeans.HL <- ddply(PSIdata.HL, .(condition,s,i), function(x) colMeans(x[locEE], na.rm = T))
+  #put (s,i) means for High and Low groups into separate dataframes
+  siMeans <- dlply(siMeans.HL, .(condition), function(x) x[-1])
+
+  #recode 'low' and 'high' conditions into -1 and 1, respectively, and make numeric variable
   PSIdata.HL$condition[PSIdata.HL$condition == hiCond] <- 1
   PSIdata.HL$condition[PSIdata.HL$condition == loCond] <- -1
   PSIdata.HL$condition <- as.numeric(PSIdata.HL$condition)
 
-  #test all of the differences for significance all at once
+  #test all of the differences for significance all at once, within each (s,i) pair
   test.r<-ddply(PSIdata.HL, .(s,i), function(x) corr.test(x$condition,x[locEE])$r)
   test.p<-ddply(PSIdata.HL, .(s,i), function(x) corr.test(x$condition,x[locEE])$p)
 
-  results <- list(test.r, test.p)
-  names(results) <- c("rXCond","pXCond")
+  results <- list(siMeans, test.r, test.p)
+  names(results) <- c("siMeans","rXCond","pXCond")
   return(results)
 
 }
--- R/strLine.R
+++ R/strLine.R
@@ -3,26 +3,24 @@
 #' This will return the percentage of situation-action pairs for which the
 #' respondent showed zero response variability.
 #'
-#'
 #' @param ESJT ESJT matrix (should have 'p','s','i' as their first three features)
-#' @param rmvLast remove the final variable (e.g., if it is Likelihood
+#' @param locX location of expected outcome ratings (note: default generously assumes
+#' the prescribed 'c(p,s,i,...,Likelihood)' data structure)
 #' ratings provided at different portion of survey)
-#' @details It is recommended within Wood, Lowman, & Harms (202x) that
-#' respondents showing 10\% straightlining by this index be removed from all analyses
+#' @details It is recommended within that respondents showing
+#' 10\% straightlining by this index be removed from all analyses,
+#' however less stringent cutoffs (e.g., 25\%) might be employed when
+#' respondents rate very few features (e.g., 5 or less) or if all features
+#' are desirable, both of which can make straight-lining behavior less suspect
 #' @return per0.p - the percentage of situation-action pairs in which
 #' the respondent showed zero response variability whatsoever.
 #' @export
 #' @examples
 #' #combine this function and recommended screen into a single statement:
-#' analysiscases <- subset(strLine(ESJT), per0.p > .25)
+#' analysiscases <- subset(strLine(ESJT), per0.p > .10)
 
-
-strLine <- function(ESJT, rmvLast=T) {
-  if(rmvLast==T){
-    varE.pi<-ddply(PSIdata, .(p,s,i), function(x) (sd(as.matrix(x[4:(ncol(x)-1)]),na.rm = T)))
-  }  else {
-    varE.pi<-ddply(PSIdata, .(p,s,i), function(x) (sd(as.matrix(x[4:ncol(x)]),na.rm = T)))
-  }
+strLine <- function(ESJT, locX=c(4:(ncol(ESJT)-1))) {
+    varE.pi<-ddply(PSIdata, .(p,s,i), function(x) (sd(as.matrix(x[locX]),na.rm = T)))
   per0.p<-ddply(varE.pi, .(p), function(x) sum(x$V1 == 0)/nrow(x))
   colnames(per0.p) <- c("p","per0.p")
   return(per0.p)
