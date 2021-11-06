ee2e63b75d62019f57bb8d7adb3576e78f6b15e5
Dustin Wood <84573697+funfield-lab@users.noreply.github.com>
2021-11-06 14:43:31 +0300

    'uMatrix2' in 'fieldResults' now is better able to handle covariance matrices (it was previously written to assume 1's on diagonal)

--- R/fieldResults.R
+++ R/fieldResults.R
@@ -16,57 +16,59 @@
 #' @details Note that the \code{uMatrix2} is a servicable means of estimating
 #' the unpredicted residuals in some of the models where lavaan does not want
 #' to provide the path-analysis implied residuals. But it should only work when
-#' the model is a DAG that has been placed to have rows in 'causal order'
+#' the model is a DAG that has been placed to have rows in 'causal order'.
+#'     There is also a problem with how \code{uMatrix2} is estimated (the final covariance
+#'     is always misestimated) which I assume
+#'     involves a bad interaction with how \code{expOutcomes} function closes out
 #' @export
 
 fieldResults <- function(fModel,covMat, sampleN = 4) {
 
-  fit_r1 <- lavaan::sem(fModel, sample.cov=covMat, sample.nobs = sampleN)
+  fit_cov1 <- lavaan::sem(fModel, sample.cov=covMat, sample.nobs = sampleN)
 
   # get the functional field matrices out of the lavaan output.
-  fittedmatrix<-fit_r1@Model@GLIST$beta
-  colnames(fittedmatrix)<-fit_r1@Model@dimNames[[1:2]]
+  fittedmatrix<-fit_cov1@Model@GLIST$beta
+  colnames(fittedmatrix)<-fit_cov1@Model@dimNames[[1:2]]
   rownames(fittedmatrix)<-colnames(fittedmatrix)
-  fieldmatrix_r1 <- t(fittedmatrix)[-1,-1]
+  fieldmatrix_cov1 <- t(fittedmatrix)[-1,-1]
+
+  #this can be used to put the original matrices into the same order specified in the model (MO).
+  MO<-colnames(fittedmatrix)[-1]
+  cov1_MO<-covMat[MO,MO]
+  res_cov1 <- as.matrix(lavaan::resid(fit_cov1)$cov) #on many occasions, this does NOT seem to provide the results that it should.
+  #summary(fit_cov1)  #Standard output if desired.
 
   #estimate total effects, crudely
   #this function should only work when the field matrix is truly a
   #DAG, with the field matrix in causal order
-  totalE <- fieldmatrix_r1 * 0
-  for(i in 1:nrow(fieldmatrix_r1)){
-    doi <- rep(0,ncol(fieldmatrix_r1))
-    doi[i] <-  sqrt(1 - diag(totalE)[i]) #activate node, but discount by the degree to which it has already been activated by other variables previously
-    effect.i<-colSums(expOutcomes(doi,fieldmatrix_r1))
+  totalE <- fieldmatrix_cov1 * 0
+  for(i in 1:nrow(fieldmatrix_cov1)){
+    doi <- rep(0,ncol(fieldmatrix_cov1))
+    doi[i] <-  sqrt(diag(cov1_MO)[i] - diag(totalE)[i]) #activate node, but discount by the degree to which it has already been activated by other variables previously
+    effect.i<-colSums(expOutcomes(doi,fieldmatrix_cov1))
     E.i<-as.matrix(effect.i)%*%t(as.matrix(effect.i)) #what are the total implied covariation from activating i?
     totalE <- totalE + E.i
   }
+  res2 <- cov1_MO - totalE
 
-  #this can be used to put the original matrices into the same order specified in the model (MO).
-  MO<-colnames(fittedmatrix)[-1]
-  r1_MO<-covMat[MO,MO]
-  res_r1 <- as.matrix(lavaan::resid(fit_r1)$cov) #on many occasions, this does NOT seem to provide the results that it should.
-  res2 <- r1_MO - totalE
-  #Standard output if desired.
-  #summary(fit_r1)
-  #d_descriptives  <- cbind(r1_MO[,1],diag(as.matrix(r1_MO)^.5))
 
   #extract the parameters
-  parTable_r1 <- lavaan::parTable(fit_r1)
-  parTable_r1$t <- parTable_r1$est/parTable_r1$se
-  parTable_r1$effNX <- sampleN
-  parTable_r1$p <- round(2*pt(-abs(parTable_r1$t),df=sampleN-1),6)
+  parTable_cov1 <- lavaan::parTable(fit_cov1)
+  parTable_cov1$t <- parTable_cov1$est/parTable_cov1$se
+  parTable_cov1$effNX <- sampleN
+  parTable_cov1$p <- round(2*pt(-abs(parTable_cov1$t),df=sampleN-1),6)
 
-  outfit_r1 <- lavaan::parameterEstimates(fit_r1)
-  outfit_r1$Neff <- sampleN
+  outfit_cov1 <- lavaan::parameterEstimates(fit_cov1)
+  outfit_cov1$Neff <- sampleN
 
 
-  #semPaths(fit_r1)
+  #semPaths(fit_cov1)
 
   #look at how well each feature in the model is predicted.
-  R_fit_r1 <- as.matrix(lavaan::inspect(fit_r1, "rsquare")^.5)
+  R_fit_cov1 <- as.matrix(lavaan::inspect(fit_cov1, "rsquare")^.5)
 
 
-  fieldResults<-list(r1_MO,fieldmatrix_r1,res_r1,res2,parTable_r1)
+  fieldResults<-list(cov1_MO,fieldmatrix_cov1,res_cov1,res2,parTable_cov1)
   names(fieldResults) <- c("eMatrix","fMatrix","uMatrix","uMatrix2","parTable")
   return(fieldResults)
 }
