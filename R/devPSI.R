f1324689357f99e10f81985001c25efa1eeb1c7c
Dustin Wood <84573697+funfield-lab@users.noreply.github.com>
2022-07-19 06:51:41 +0300

    switch 'min_i' default in devPSI from T to F

--- R/devPSI.R
+++ R/devPSI.R
@@ -7,12 +7,15 @@
 #' @param locX location of expected outcome ratings (note: default generously assumes
 #' the prescribed 'c(p,s,i,...,Likelihood)' data structure)
 #' ratings provided at different portion of survey)
+#' @param min_i Return only the minimum value of i (often i=1)?
 #' @details This essentially forces 'deviation' scoring.  It also assumes that
 #' 'i' is coded as a numerical variable, and if 'min_i' is selected will return only
 #' the actions with the lowest value (so if coded as '1,2,3...' then will only return '1';
 #' if '2.1,2.2,2.3,...' will only return '2.1', and so on). When nI = 2 for each
 #' situation, deviation scoring will force i=1 and i=2 to be perfect reflections of one
-#' another, so only one (i=1) is retained.
+#' another, so only one (i=1) is retained.  This can be useful to help avoid artificial
+#' inflation of sample size (although other strategies, like multilevel modeling in lavaan,
+#' may be preferable to address this issue).
 #'
 #' Also note that in this case, if the deviations are doubled, you will obtain the
 #' simple difference between i=1 and i=2 for each feature.
@@ -20,7 +23,7 @@
 #' from the person's average rating of actions in that situation.
 #' @export
 
-devPSI <- function(PSIdata, locX=c(4:ncol(PSIdata)),min_i = T) {
+devPSI <- function(PSIdata, locX=c(4:ncol(PSIdata)),min_i = F) {
   #make sure everything is sorted before doing dangerous merge later
   PSIdata <- with(PSIdata, PSIdata[order(p,s,i),])
 
