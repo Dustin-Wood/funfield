9de66b6fc28edc3c0cd7e8ccb55645bc5cb394d8
Dustin Wood <84573697+Dustin-Wood@users.noreply.github.com>
2023-06-10 12:35:00 +0300

    separated devPSI and diffPSI functions and made the latter a bit more flexible (can select either minimum or maximum i, as desired).

--- R/devPSI.R
+++ R/devPSI.R
@@ -7,29 +7,23 @@
 #' @param locX location of expected outcome ratings (note: default generously assumes
 #' the prescribed 'c(p,s,i,...,Likelihood)' data structure)
 #' ratings provided at different portion of survey)
-#' @param max_i Return only the maximum value of i (often i=1)
-#' @param dbl Double the deviation scores
-#' (this will mainly make sense to do if nI = 2 and max_i = T,
-#' in which case scores will be difference scores rather than deviation scores)
 #' @examples (develop some example code)
-#' @details This essentially forces 'deviation' scoring.  It also assumes that
-#' 'i' is coded as a numerical variable, and if 'max_i' is selected, this will only return
-#' the actions with the highest value.  It is recommended this is only used when
-#' have been coded where i = 1 for the focal action, i = 0 for a contrasting action,
-#' and other actions i have been excluded (maybe by subsetting), so that nI = 2.
+#' @details This essentially forces 'deviation' scoring.
+#'
 #' Note that when nI = 2, the deviation scores will be reflections of one another.
 #' In which case, keeping only the maximum i (which should be i=1)  can be useful
 #' to help avoid artificial inflation of sample size
 #' (although other strategies, like multilevel modeling in lavaan,
 #' may be preferable to address this issue).
 #'
-#' Also note that in this case, if the deviations are doubled, you will obtain the
-#' simple difference between i=1 and i=2 for each feature.
+#' If we want the difference of the two actions (rather than deviation scoring),
+#' we can pair the devPSI function with diffPSI.
+#'
 #' @return deviated ratings of effects of action i as rated by p within situation s
 #' from the person's average rating of actions in that situation.
 #' @export
 
-devPSI <- function(PSIdata, locX=c(4:ncol(PSIdata)),max_i = F,dbl = F) {
+devPSI <- function(PSIdata, locX=c(4:ncol(PSIdata))) {
   #make sure everything is sorted before doing dangerous merge later
   PSIdata <- with(PSIdata, PSIdata[order(p,s,i),])
 
@@ -40,13 +34,5 @@ devPSI <- function(PSIdata, locX=c(4:ncol(PSIdata)),max_i = F,dbl = F) {
   #(dangerous merge)
   devPSI <- data.frame(PSIdata[1:3],devPS[3:ncol(devPS)])
 
-  #optional return only first action
-  if(max_i == T) {
-    devPSI<-ddply(devPSI, .(p,s), function(x) subset(x, i == max(i)))
-  }
-
-  if(dbl == T) {
-    devPSI[4:ncol(devPSI)] <- devPSI[4:ncol(devPSI)]*2
-  }
   return(devPSI)
 }
---
+++ R/diffPSI.R
@@ -0,0 +1,38 @@
+#' Difference Scoring of nI=2 PSI matrix
+#' @description
+#' Will either return the difference Will estimate how p's rating of outcomes of each action deviate within situation s
+#' deviated from their average rating of outcomes of different actions in that situation
+#'
+#' @param devPSI deviated PSI-structured dataframe (should have 'p','s','i' as their first three features)
+#' @param locX location of expected outcome ratings (default generously assumes
+#' the prescribed 'c(p,s,i,...,Likelihood)' data structure)
+#' @param select_i Return the maximum or minimum of i?
+#' @param dbl Double the deviation scores (default is T)
+#' @details Note that when nI = 2, the dpsi scores will be reflections of one another.
+#' In which case, keeping only the minimum or maximum i for each scenario can be useful
+#' to help avoid artificial inflation of sample size
+#' (although other strategies, like multilevel modeling in lavaan,
+#' may be preferable to address this issue).
+#'
+#' When nI is greater than 2, a more sophisticated script should be used, which perhaps subtracts the
+#' focal action (e.g., A = 1) from the average of the contrasting actions (e.g., A = 0)
+#' @return expected effects (difference) of focal action(s) from contrasting action(s)
+#' @export
+
+diffPSI <- function(devPSI, locX=c(4:ncol(PSIdata)),select_i = "max",dbl = T) {
+
+  #optional return only first action
+  if(select_i == "max") {
+    diffPSI<-ddply(devPSI, .(p,s), function(x) subset(x, i == max(i)))
+  }
+
+  #optional return only first action
+  if(select_i == "min") {
+    diffPSI<-ddply(devPSI, .(p,s), function(x) subset(x, i == min(i)))
+  }
+
+  if(dbl == T) {
+    diffPSI[4:ncol(devPSI)] <- diffPSI[4:ncol(diffPSI)]*2
+  }
+  return(diffPSI)
+}
